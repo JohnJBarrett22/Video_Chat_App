3c9996affbb06f6de96e5048dfa89b1461112e55
John Barrett <johnjbarrett22@gmail.com>
2020-10-10 23:22:29 -0700

    Set up server basics, ejs/css files and beginning to flesh out the app

---
+++ server.js
@@ -0,0 +1,21 @@
+//Imports
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const {v4: uuidV4} = require('uuid')
+
+//Config
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+
+//Routes
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4}()`)
+})
+app.get('/:room', (req, res) => {
+    res.render('room', {roomId: req.params.room})
+})
+
+server.listen(1337)
