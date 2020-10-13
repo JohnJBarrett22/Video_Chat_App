fad3bf12787090fba9acc8bef85455b1b079a019
John Barrett <johnjbarrett22@gmail.com>
2020-10-13 03:03:05 -0700

    Added very rough chat functionality that is plagued with errors, lots of refactoring needed

--- public/script.js
+++ public/script.js
@@ -6,8 +6,12 @@ const myPeer = new Peer(undefined, {
 })
 const myVideo = document.createElement('video')
 const peers = {}
+const chatbox = document.getElementById("chatbox")
+let name = prompt("Please enter your name:","Name")
+let msg = ""
 
 
+//Video Features
 myVideo.muted = true
 navigator.mediaDevices.getUserMedia({
     video: true,
@@ -22,7 +26,7 @@ navigator.mediaDevices.getUserMedia({
             addVideoStream(video, userVideoStream)
         })
     })
-
+    socket.emit("new_user", {name:name})
     socket.on('user-connected', userId => {
         connectToNewUser(userId, stream)
     })
@@ -31,12 +35,10 @@ navigator.mediaDevices.getUserMedia({
 socket.on('user-disconnected', userId => {
     if (peers[userId]) peers[userId].close()  
 })
-    
 
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id)
 })
-
 
 function connectToNewUser(userId, stream) {
     const call = myPeer.call(userId, stream)
@@ -50,11 +52,35 @@ function connectToNewUser(userId, stream) {
     peers[userId] = call
 }
 
-
 function addVideoStream(video, stream) {
     video.srcObject = stream
     video.addEventListener('loadedmetadata', () => {
         video.play()
     })
     videoGrid.append(video)
+}
+
+
+//Chat Box Features
+socket.on("display_new_user", data => {
+    document.getElementById("chatbox").append("<p> style='color:green'>"+data.name+" has joined the chat"+"</p>")
+})
+
+socket.on("existing_messages", data => {
+    for(i in data){
+        document.getElementById("chatbox").append("<p>"+data[i].name+": "+data[i].message+"</p>")
+        chatbox.scrollTop = chatbox.scrollHeight;
+    }
+})
+
+socket.on("update_messages", data => {
+    document.getElementById("chatbox").append("<p>"+data.name+": "+data.message+"</p>")
+    chatbox.scrollTop = chatbox.scrollHeight;
+})
+    
+
+function newMsgSent() {
+    msg = document.getElementById("msg").value
+    socket.emit("new_message", {name:name, message:msg})
+    document.getElementById("msg").value = "";
 }
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const {v4: uuidV4} = require('uuid')
+let messages = {};
+let users = {};
+let id = 0
 
 //Config
 app.set('view engine', 'ejs')
@@ -22,11 +25,21 @@ io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         console.log(roomId, userId)
         socket.join(roomId)
-        socket.to(roomId).broadcast.emit('user-connected', userId)
-        
         socket.on('disconnect', () => {
             socket.to(roomId).broadcast.emit('user-disconnected', userId)
         })
+    })
+
+    socket.on('new_user', (data) => {
+        users[socket.id] = {name:data.name}
+        socket.emit("existing_messages", messages)
+        io.emit("display_new_user", {name:data.name})
+    })
+
+    socket.on("new_message", (data) => {
+        messages[id] = {name:data.name, message:data.message}
+        io.emit("update_messages", messages[id])
+        id++;
     })
 })
 
